f6e577ca831243cd9fe72402bcef4c03384a0a66
DevanshSriv <devanshsrivastava@cedcoss.com>
2022-05-09 18:22:06 +0530

    chess boards

--- src/index.php
+++ src/index.php
@@ -1,12 +1,17 @@
 <?php
-function fact($a){
-    $f=1;
-    for($i=1;$i<=$a;$i++){
-        $f*=$i;
+echo "<table border=1px solid black>";
+for($row=1;$row<=8;$row++){
+    echo "<tr>";
+    for($col=1;$col<=8;$col++){
+        if(($row+$col)%2==0){
+            echo "<td width=30px height=30px bgcolor=white></td>";
+        }
+        else{
+            echo "<td width=30px height=30px bgcolor=black></td>";
+        }
     }
-    echo $f;
+    echo "</tr>";
 }
-fact(5);
-
+echo "</table>";
 
 ?>
