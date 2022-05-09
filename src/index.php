b985fef775b0ba868dc8923f393a5f3c59147b59
DevanshSriv <devanshsrivastava@cedcoss.com>
2022-05-09 17:57:15 +0530

    checking grades of student

--- src/index.php
+++ src/index.php
@@ -1,7 +1,19 @@
 <?php
-$a='PHP';
-$b=44;
-echo "Welcome to ".$a." ".$b;
+function grades($a){
+    if($a>60){
+        echo "First Division";
+    }
+    else if($a>=45 && $a<=59){
+        echo "Second Division";
+    }
+    else if($a>=33 && $a<=44){
+        echo "Third Division";
+    }
+    else if($a<33){
+        echo "Fail";
+    }
+}
+grades(28);
 
 
 ?>
