46606132f6dc360dbef29af94ded5bcfb06d4f14
DevanshSriv <devanshsrivastava@cedcoss.com>
2022-05-10 15:25:44 +0530

    Table print of PHP array

--- src/index.php
+++ src/index.php
@@ -1,10 +1,111 @@
 <?php
-$arr=array(1,2,3,4,5,6,7,8);
-$rem=5;
-$result= array_filter($arr,function($value) use($rem){
-return $value!=$rem;
-});
-foreach($result as $r){
-    echo " ".$r;
+$products = array(
+    "Electronics" => array(
+      "Television" => array(
+        array(
+        "id" => "PR001",
+        "name" => "MAX-001",
+        "brand" => "Samsung"
+        ),
+        array(
+        "id" => "PR002",
+        "name" => "BIG-301",
+        "brand" => "Bravia"
+        ),
+        array(
+        "id" => "PR003",
+        "name" => "APL-02",
+        "brand" => "Apple"
+        )
+      ),
+      "Mobile" => array(
+        array(
+        "id" => "PR004",
+        "name" => "GT-1980",
+        "brand" => "Samsung"
+        ),
+        array(
+        "id" => "PR005",
+        "name" => "IG-5467",
+        "brand" => "Motorola"
+        ),
+        array(
+        "id" => "PR006",
+        "name" => "IP-8930",
+        "brand" => "Apple"
+        )
+      )
+    ),
+    "Jewelry" => array(
+      "Earrings" => array(
+        array(
+        "id" => "PR007",
+        "name" => "ER-001",
+        "brand" => "Chopard"
+        ),
+        array(
+        "id" => "PR008",
+        "name" => "ER-002",
+        "brand" => "Mikimoto"
+        ),
+        array(
+        "id" => "PR009",
+        "name" => "ER-003",
+        "brand" => "Bvlgari"
+        )
+      ),
+      "Necklaces" => array(
+        array(
+        "id" => "PR010",
+        "name" => "NK-001",
+        "brand" => "Piaget"
+        ),
+        array(
+        "id" => "PR011",
+        "name" => "NK-002",
+        "brand" => "Graff"
+        ),
+        array(
+        "id" => "PR012",
+        "name" => "NK-003",
+        "brand" => "Tiffany"
+        )
+      )
+    )
+        );
+
+echo "<table border=1px solid black>";
+echo '<tr>';
+ 
+echo '<td>Catogry</td>';
+echo '<td>Sub-Catogry</td>';
+echo '<td>ID</td>';
+echo '<td>Name</td>';
+echo '<td>Brand</td>';
+echo '</tr>';
+
+foreach($products as $prKey => $catogry){
+    echo'<tr>';
+   
+    foreach($catogry as $subCat=>$subVal)
+    {
+       
+    foreach($subVal as $subAr=>$ar)
+    {   echo '<td>'.$prKey.'</td>';
+        echo '<td>'.$subCat.'</td>';
+        foreach($ar as $rem)
+        {
+        echo '<td>'.$rem.'</td>';
+        
+        }
+       
+        echo '</tr>';
+    
+    }
+    
+   }
 }
+    
+echo '</table>';
+ 
 ?>
