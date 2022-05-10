1de1e30e9c1e752c95ca989126cdaae1eb37de61
DevanshSriv <devanshsrivastava@cedcoss.com>
2022-05-10 11:07:38 +0530

    insert element at any position

--- src/index.php
+++ src/index.php
@@ -2,12 +2,19 @@
 function insert($pos,$val){
    static $arr=array(1,2,3,4,5,6);
 
-   array_splice($arr,$pos,0,$val);
-   var_dump($arr);
+   echo "Original Array:"."<br>";
+   foreach($arr as $x){
+       echo $x." ";
+   }
+
+   array_splice($arr,$pos-1,0,$val);
+   echo "<br>After inserting:"."<br>";
+   foreach($arr as $x){
+       echo $x." ";
+   }
    
 }
 insert(3,44);
-echo "<br>";
-insert(1,77);
+
 
 ?>
