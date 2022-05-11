3b674d2f095b3b5f4d864ba2079c1c0a6133be3f
DevanshSriv <devanshsrivastava@cedcoss.com>
2022-05-11 14:35:55 +0530

    Time conversion

--- src/index.php
+++ src/index.php
@@ -1,37 +1,29 @@
-<?php
-$len=$wid=$area=$perimeter=0;
-$ubitErr2=$unitErr1='';
+<?php 
+$err=$res='';
+$cal=$in=0;
 if(isset($_POST)){
-   if(empty($_POST['length'])){
-    $unitErr1='required';
-   }
-   if(empty($_POST['width'])){
-     $unitErr2='required';
-    }
+  if(empty($_POST['input'])){
+    $err='Cannot Be Empty';
+  }
   else{
-    $len=test($_POST['length']);
-    $wid=test($_POST['width']);
-    if(!preg_match('/^[0-9]+$/',$num1)){
-      $unitErr1='Invalid Unit';
+    $in=$_POST['input'];
+    if(preg_match('/^[A-Za-z]+$/',$in)){
+      $err='Only Numbers';
     }
-    elseif(!preg_match('/^[0-9]+$/',$num2)){
-      $unitErr2='Invalid Unit';
-    }
-    
-     $area= $len*$wid;
-     $perimeter=2*($len+$wid);
-    
-   }
-}
+    else{
+      $c=$_POST['choice'];
+      if($c=='mins'){
+        $cal=$in*60;
+        $res=$in.'Hr = '.$cal.'mins';
+      }
+      else{
+        $cal=$in*3600;
+        $res=$in.'Hr = '.$cal.'sec';
+      }
+    } 
+  }
 
-  
-function test($data){
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -41,25 +33,20 @@ function test($data){
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <style>
-  .error{color:red;}
-  </style>
+  <style>.error{color:red;}</style>
 </head>
 <body>
-  <div style='display:flex;flex-direction:coloumn;justify-content:center;margin:5%;padding:5%;'>
-  <form method='POST' action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> align ='center'>
-    Length Of Rectangle: <input type='text' name='length'>mtr
-    <br><br>
-    Length Of Width: <input type='text' name='width'>mtr
-    <br><br>
-    <input type='submit' value='Calculate Area And Perimeter' style='padding:2%;background-color:grey;border-radius:5px'>
-     
+  <div style='display:flex;flex-direction:coloumn;justify-content:center;'>
+    <form action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>'method='POST'>
+     <input type='text' placeholder='Enter Hours' name='input'><span class='error'>*<?php echo $err; ?></span><br><br>
+     <input type='radio' name='choice' value='mins'>Hours to Min<br><br>
+     <input type='radio' name='choice' value='sec'>Hours to Second<br>
+     <br><br>
+      <?php echo $res; ?><br><br>
+      <input type='submit'  value='CONVERT'> 
   </form>
-  
-  </div>
-  <div style=' margin:5%;float:left;'>
-<p>Area Of Rectangle: <?php echo $area." sq mtr"; ?></p>
-<p>Perimeter Of Rectangle: <?php echo $perimeter.' mtr'; ?></p>
-</div> 
+ 
+</div>
+
 </body>
 </html>
